4d17e73f2152f455ee50ae79f9b1b94a356d3929
Xiaoyi Liu <lxy878@gmail.com>
2020-07-11 13:36:41 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,9 @@ class UsersController < ApplicationController
 
   def create
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :password, :password_confirmation)
+    end
 end
