0bff2418a814300c5d331270a2990322b7fb184e
Xiaoyi Liu <lxy878@gmail.com>
2020-07-11 15:08:34 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
     puts 'create_section'
     @user = User.new(user_params)
     if @user.save
-      session[user_id]= @user.id
+      session[:user_id]= @user.id
       redirect_to root_path
     else
       render :new
