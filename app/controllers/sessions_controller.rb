b151ea882048435e8e06f6fb2ac13e5da4594846
Xiaoyi Liu <lxy878@gmail.com>
2020-07-11 14:10:46 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(name: params[:name])
     redirect_to root if !@user.authenticate(params[:password])
     session[:name]=user.name
+    
   end
 
   def destroy
