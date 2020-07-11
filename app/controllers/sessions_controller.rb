db0f2f4ef8693a925e6540b3bdad7165c9405ba7
Xiaoyi Liu <lxy878@gmail.com>
2020-07-11 14:54:26 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(name: params[:name])
     redirect_to root_path if !user.authenticate(params[:password])
-    session[:name]=user.name
+    session[:user_id]=user.name
     redirect_to users_show_path
   end
 
