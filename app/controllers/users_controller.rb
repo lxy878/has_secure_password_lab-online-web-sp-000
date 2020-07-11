28141d1dec014059a8584503c7cfc5e0fcb6cbef
Xiaoyi Liu <lxy878@gmail.com>
2020-07-11 15:11:06 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
       session[:user_id]= @user.id
       redirect_to root_path
     else
-      redirect_to '/users/new'
+      redirect_to users_new_path
     end
   end
 
